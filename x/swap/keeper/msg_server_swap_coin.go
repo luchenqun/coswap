2ed70440e4b04f914cdabca36a9a5b07daac2911
luke <luchenqun@qq.com>
2023-06-08 21:17:18 +0800

    complete SwapCoin

--- x/swap/keeper/msg_server_swap_coin.go
+++ x/swap/keeper/msg_server_swap_coin.go
@@ -2,16 +2,63 @@ package keeper
 
 import (
 	"context"
-
 	"coswap/x/swap/types"
+	"errors"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 func (k msgServer) SwapCoin(goCtx context.Context, msg *types.MsgSwapCoin) (*types.MsgSwapCoinResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	creator, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		return nil, err
+	}
+
+	if !(msg.AmountIn.Denom == "eth" && msg.AmountOutMin.Denom == "btc" || msg.AmountIn.Denom == "btc" && msg.AmountOutMin.Denom == "eth") {
+		return nil, errors.New("Only supports the exchange of ETH and BTC coin pairs")
+	}
+
+	var amountOut sdk.Coin
+
+	if msg.AmountIn.Denom == "eth" {
+		if msg.AmountIn.Amount.GTE(sdk.NewInt(10)) {
+			amountOut = sdk.NewCoin("btc", msg.AmountIn.Amount.QuoRaw(10))
+		} else {
+			return nil, errors.New("amount in eth amount must >= 10")
+		}
+	}
+
+	if msg.AmountIn.Denom == "btc" {
+		if msg.AmountIn.Amount.GT(sdk.NewInt(0)) {
+			amountOut = sdk.NewCoin("eth", msg.AmountIn.Amount.MulRaw(10))
+		} else {
+			return nil, errors.New("amount in btc amount must > 0")
+		}
+	}
+
+	if amountOut.IsGTE(msg.AmountOutMin) {
+		err = k.bankKeeper.SendCoinsFromModuleToAccount(ctx, types.ModuleName, creator, sdk.Coins{amountOut})
+		if err != nil {
+			return nil, err
+		}
+
+		err = k.bankKeeper.SendCoinsFromAccountToModule(ctx, creator, types.ModuleName, sdk.Coins{msg.AmountIn})
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		return nil, errors.New("amount out less than amount out min")
+	}
+
+	ctx.EventManager().EmitEvent(
+		sdk.NewEvent(
+			sdk.EventTypeMessage,
+			sdk.NewAttribute(sdk.AttributeKeyModule, types.ModuleName),
+			sdk.NewAttribute(sdk.AttributeKeySender, msg.Creator),
+			sdk.NewAttribute("amount-out", amountOut.String()),
+		),
+	)
 
 	return &types.MsgSwapCoinResponse{}, nil
 }
